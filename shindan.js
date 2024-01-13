7d04fa903e65a2765afb64f42f7ce59b6904fde3
Rugg-kumamoto <akinori.sonoda@gmail.com>
2024-01-13 14:08:40 +0900

    20240113

--- shindan.js
+++ shindan.js
@@ -5,12 +5,12 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-console.log("性格診断プログラム");
+console.log("自分にあったスポーツを診断してくれるプログラム");
 console.log("--------------------");
 
-rl.question("1. 体を動かすのは好きですか？ はい or いいえ ", (answer1) => {
-  rl.question("2. たくさんの人同じ空間を過ごすことは好きですか？ はい or いいえ ", (answer2) => {
-    rl.question("3. 常に紳士的な態度で人と接していますか？ はい or いいえ ", (answer3) => {
+rl.question("1. 一人が好きか？ はい or いいえ ", (answer1) => {
+  rl.question("2. 人と対峙してプレイしたいか？ はい or いいえ ", (answer2) => {
+    rl.question("3. コツコツとスキルを磨きたいか？ はい or いいえ ", (answer3) => {
       // ここでユーザーの回答をもとに性格を診断
       const personality = diagnosePersonality(answer1, answer2, answer3);
       console.log("--------------------");
@@ -25,14 +25,20 @@ function diagnosePersonality(answer1, answer2, answer3) {
   // ここで回答に応じた性格を診断し、適切なメッセージを返すロジックを記述します
   // この例では簡単な診断ロジックを示していますが、実際の診断はもっと複雑で詳細になります
   if (answer1.toLowerCase().includes("はい") && answer2.toLowerCase().includes("はい") && answer3.toLowerCase().includes("はい")) {
-    return "あなたはラガーマンの素質があります！";
-  } else if (answer1.toLowerCase().includes("いいえ") && answer2.toLowerCase().includes("はい") && answer3.toLowerCase().includes("はい")) {
-    return "あなたはスーパー飲み会幹事の素質があります！";
-  } else if (answer1.toLowerCase().includes("いいえ") && answer2.toLowerCase().includes("いいえ") && answer3.toLowerCase().includes("いいえ")) {
-    return "あなたは孤高の仕事人の素質があります";
+    return "あなたにおすすめのスポーツは卓球やバドミントン！";
+  } else if (answer1.toLowerCase().includes("はい") && answer2.toLowerCase().includes("はい") && answer3.toLowerCase().includes("いいえ")) {
+    return "あなたにおすすめのスポーツ格闘技！";
   } else if (answer1.toLowerCase().includes("はい") && answer2.toLowerCase().includes("いいえ") && answer3.toLowerCase().includes("いいえ")) {
-    return "あなたは人を騙す人になるかも！要注意！";
-  } else {
-    return "その他の回答はこちら→https://www.instagram.com/rugg_photo/";
+    return "あなたにおすすめのスポーツ陸上トラック競技";
+  } else if (answer1.toLowerCase().includes("いいえ") && answer2.toLowerCase().includes("いいえ") && answer3.toLowerCase().includes("いいえ")) {
+    return "あなたにおすすめのスポーツ駅伝！";
+  } else if (answer1.toLowerCase().includes("いいえ") && answer2.toLowerCase().includes("はい") && answer3.toLowerCase().includes("はい")) {
+    return "あなたにおすすめのスポーツサッカーやラグビー、バスケットなど！";
+  } else if (answer1.toLowerCase().includes("いいえ") && answer2.toLowerCase().includes("はい") && answer3.toLowerCase().includes("いいえ")) {
+    return "あなたにおすすめのスポーツカバディ！";
+  } else if (answer1.toLowerCase().includes("いいえ") && answer2.toLowerCase().includes("いいえ") && answer3.toLowerCase().includes("はい")) {
+    return "あなたにおすすめのスポーツ日本体育大学の集団行動！";
+  }{
+    return "このプログラムを作ったのは私です→https://www.instagram.com/rugg_photo/";
   }
 }
